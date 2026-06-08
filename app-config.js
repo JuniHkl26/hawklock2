9a673811d7b02c5cc9bbe04a536708ad82b32cc8
JuniHkl26 <leonardo1127123@gmail.com>
2026-06-08 12:29:40 -0500

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,6 +1,3 @@
-// ⚠️ IMPORTANTE: Mantén este archivo privado y NO compartas la API Key públicamente.
-// En producción, rota esta llave periódicamente.
-
 window.APP_CONFIG = {
     GS_URL: 'https://script.google.com/macros/s/AKfycbxGcZw93RbejPcudisZNlDD1qAp9FKCFDtldBHu-SXfKe9DMpNBAY5qYXWYTK8IrBMd/exec',
     GS_API_KEY: '7LIxQTkPpQzbjOU6WjRuFPtjxFGOyb6c'
