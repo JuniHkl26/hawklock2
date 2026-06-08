999e9b17a36c503c13a2d4e220e5242c2e428787
JuniHkl26 <leonardo1127123@gmail.com>
2026-06-08 18:22:11 -0500

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-    GS_URL: 'https://script.google.com/macros/s/AKfycby0J0kzVDsHASiSB1UwIe--I6fwPrsBbCA7_HNYbBxUOqvWi2RXmsK6Z4BkCUKuPaRe/exec'
+    GS_URL: 'https://script.google.com/macros/s/AKfycbwEFl9-buYeIIF4c6u0S-_sTVsaTkBwZxyg7so1VcoVFQBOy3modRh7t2nimxR38O9AdQ/exec'
 };
